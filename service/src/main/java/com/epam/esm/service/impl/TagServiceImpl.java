8e8986d69ac694d22c0ec91c92de1ae23f1a3d20
Karina Radyuk <radyuk.karina@bk.ru>
2022-05-09 17:41:26 +0300

    fix: add check if tag exists

--- service/src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
@@ -2,12 +2,15 @@ package com.epam.esm.service.impl;
 
 import com.epam.esm.dto.TagDto;
 import com.epam.esm.entity.Tag;
+import com.epam.esm.exception.TagAlreadyExistsException;
 import com.epam.esm.exception.TagNotFoundException;
 import com.epam.esm.mapper.DtoMapper;
 import com.epam.esm.repository.TagRepository;
 import com.epam.esm.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +34,11 @@ public class TagServiceImpl implements TagService {
 
     @Override
     public void saveTag(TagDto tagDto) {
-        tagRepository.saveEntity(tagDtoMapper.mapToEntity(tagDto));
+        Optional<Tag> optionalTag = tagRepository.findById(tagDto.getId());
+        if (optionalTag.isPresent()) {
+            throw new TagAlreadyExistsException(tagDto.getName());
+        } else {
+            tagRepository.saveEntity(tagDtoMapper.mapToEntity(tagDto));
+        }
     }
 }
