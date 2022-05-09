c13c93eb8dc8d35214ead3123ef1a0a40d578299
Karina Radyuk <radyuk.karina@bk.ru>
2022-05-09 13:18:04 +0300

    feat: add User & Order services

---
+++ service/src/main/java/com/epam/esm/service/impl/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package com.epam.esm.service.impl;
+
+import com.epam.esm.dto.OrderDto;
+import com.epam.esm.entity.Order;
+import com.epam.esm.mapper.DtoMapper;
+import com.epam.esm.repository.OrderRepository;
+import com.epam.esm.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    private final DtoMapper<Order, OrderDto> orderDtoMapper;
+
+    @Override
+    public OrderDto retrieveSingleOrder(Long id) {
+        return orderRepository.findById(id).map(orderDtoMapper::mapToDto)
+                .orElseThrow(RuntimeException::new); //TODO change exception
+    }
+}
---
+++ service/src/main/java/com/epam/esm/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.epam.esm.service.impl;
+
+import com.epam.esm.dto.UserDto;
+import com.epam.esm.entity.User;
+import com.epam.esm.mapper.DtoMapper;
+import com.epam.esm.repository.UserRepository;
+import com.epam.esm.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    private final DtoMapper<User, UserDto> userDtoMapper;
+
+    @Override
+    public UserDto retrieveSingleUser(Long id) {
+        return userRepository.findById(id).map(userDtoMapper::mapToDto)
+                .orElseThrow(RuntimeException::new);//TODO change exception
+    }
+}
